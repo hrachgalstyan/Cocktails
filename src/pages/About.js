6c22ae975e8b52b675d011dc7db15049da2e93f1
hrachgalstyan <hrach_002@mail.ru>
2020-05-07 01:40:22 +0400

    finished

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function About() {
+  return <section className="section about-section">
+    <h1 className="section-title">about us</h1>
+    <p>Consequat ad Lorem laboris velit ut sunt reprehenderit cillum veniam ex adipisicing irure deserunt. Mollit amet Lorem cupidatat id ad id amet sit. In ea ad deserunt nulla dolor anim aute ullamco adipisicing aliquip consequat proident. Fugiat in elit consectetur voluptate deserunt aliqua laboris cillum amet excepteur nostrud id. Velit dolor do aute consequat esse amet proident veniam occaecat cupidatat deserunt Lorem. Magna enim pariatur est minim esse fugiat cillum ipsum eu consectetur. Consequat veniam incididunt id non aliquip.</p>
+  </section>;
+}
